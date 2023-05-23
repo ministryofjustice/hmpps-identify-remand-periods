5ebef72a066f677e280ad59fdb18f3a80f75353f
Luke Harper <luke.harper@digital.justice.gov.uk>
2023-05-23 13:34:20 +0100

    MDWFP-129 fix redirect issue.

--- server/routes/remandRoutes.ts
+++ server/routes/remandRoutes.ts
@@ -52,26 +52,12 @@ export default class RemandRoutes {
         days: relevantRemand.sentenceRemand.map(it => it.days).reduce((sum, current) => sum + current, 0),
       }),
     )
-    return res.redirect(`${config.services.adjustmentServices.url}/adjustments/${nomsId}`)
+    return res.redirect(`/${nomsId}/adjustments`)
   }
 
-  public submitRemand: RequestHandler = async (req, res): Promise<void> => {
-    const { token } = res.locals.user
+  public redirectToAdjustmentsService: RequestHandler = async (req, res): Promise<void> => {
     const { nomsId } = req.params
-
-    const relevantRemand = (await this.identifyRemandPeriodsService.calculateRelevantRemand(nomsId, token))
-      .sentenceRemand
-    const adjustment: PrisonApiAdjustment = {
-      from: relevantRemand[0].from,
-      to: relevantRemand[0].to,
-      type: 'REMAND',
-      days: relevantRemand[0].days,
-      sequence: relevantRemand[0].charge.sentenceSequence,
-    }
-
-    await this.prisonerService.createAdjustment(relevantRemand[0].charge.bookingId, adjustment, token)
-
-    return res.redirect(`/remand/${nomsId}`)
+    return res.redirect(`${config.services.adjustmentServices.url}/${nomsId}`)
   }
 
   public bulkRemand: RequestHandler = async (req, res): Promise<void> => {
