36d231dbffe9acd540bfb275ed36f61e5c9cbc8e
ldlharper <luke.harper@digital.justice.gov.uk>
2024-10-04 10:00:39 +0100

    ADJST-863 use inactive booking role to see remand for inactive prisoner. (#128)

--- server/services/prisonerSearchService.test.ts
+++ server/services/prisonerSearchService.test.ts
@@ -4,6 +4,7 @@ import config from '../config'
 import PrisonerSearchService from './prisonerSearchService'
 import { PrisonerSearchApiPrisoner } from '../@types/prisonerSearchApi/prisonerSearchTypes'
 import FullPageErrorType from '../model/FullPageErrorType'
+import { UserDetails } from './userService'
 
 jest.mock('../data/hmppsAuthClient')
 
@@ -35,16 +36,50 @@ describe('Prisoner search service related tests', () => {
   it('Test getting prisoner details', async () => {
     fakeApi.get(`/prisoner/A1234AB`).reply(200, prisonerDetails)
 
-    const result = await prisonerSearchService.getPrisonerDetails('A1234AB', ['MDI'], username)
+    const result = await prisonerSearchService.getPrisonerDetails('A1234AB', {
+      username,
+      caseloads: ['MDI'],
+      roles: [],
+    } as UserDetails)
 
     expect(result).toEqual(prisonerDetails)
+  })
+  it('Test getting prisoner details OUT without inactive booking role', async () => {
+    fakeApi.get(`/prisoner/A1234AB`).reply(200, { ...prisonerDetails, prisonId: 'OUT' })
+
+    try {
+      await prisonerSearchService.getPrisonerDetails('A1234AB', {
+        username,
+        caseloads: ['MDI'],
+        roles: [],
+      } as UserDetails)
+    } catch (error) {
+      expect(error.errorKey).toBe(FullPageErrorType.NOT_IN_CASELOAD)
+      expect(error.status).toBe(404)
+    }
+  })
+
+  it('Test getting prisoner details OUT with inactive booking role', async () => {
+    fakeApi.get(`/prisoner/A1234AB`).reply(200, { ...prisonerDetails, prisonId: 'OUT' })
+
+    const result = await prisonerSearchService.getPrisonerDetails('A1234AB', {
+      username,
+      caseloads: ['MDI'],
+      roles: ['INACTIVE_BOOKINGS'],
+    } as UserDetails)
+
+    expect(result).toEqual({ ...prisonerDetails, prisonId: 'OUT' })
   })
 
   it('Test getting prisoner details when caseload is different', async () => {
     fakeApi.get(`/prisoner/A1234AB`).reply(200, { ...prisonerDetails, agencyId: 'LEX' })
 
     try {
-      await prisonerSearchService.getPrisonerDetails('A1234AB', ['MDI'], username)
+      await prisonerSearchService.getPrisonerDetails('A1234AB', {
+        username,
+        caseloads: ['MDI'],
+        roles: [],
+      } as UserDetails)
     } catch (error) {
       expect(error.errorKey).toBe(FullPageErrorType.NOT_IN_CASELOAD)
       expect(error.status).toBe(404)
